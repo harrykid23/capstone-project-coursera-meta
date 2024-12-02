916769459a87092f3ab5aa1528448ad5aa72e602
harrykid23 <kidharry23@gmail.com>
2024-12-02 23:12:39 +0700

    update unit testing #2

--- src/App.test.js
+++ src/App.test.js
@@ -1,29 +1,27 @@
 import { render, screen } from "@testing-library/react";
 import BookingForm from "./components/BookingForm";
 import { initializeTimes, updateTimes } from "./components/Main";
+import { MemoryRouter } from "react-router";
 
 test("Renders the Booking Form heading", () => {
-  render(<BookingForm availableTimes={[]} />);
+  render(
+    <MemoryRouter>
+      <BookingForm availableTimes={[]} dispatchAvailableTimes={() => {}} />
+    </MemoryRouter>
+  );
   const headingElement = screen.getByText("Booking Form");
   expect(headingElement).toBeInTheDocument();
 });
 
 test("initializeTimes working correctly", () => {
-  const result = initializeTimes();
-  expect(result).toEqual([
-    "17:00",
-    "18:00",
-    "19:00",
-    "20:00",
-    "21:00",
-    "22:00",
-  ]);
+  const result = initializeTimes(new Date());
+  expect(result?.length).toBeGreaterThanOrEqual(1);
 });
 
 test("updateTimes working correctly", () => {
   const state = {
     data: ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"],
   };
-  const result = updateTimes(state);
-  expect(result).toEqual(state);
+  const result = updateTimes(state, { date: new Date("2025-01-23") });
+  expect(result.data?.length).toBeGreaterThanOrEqual(1);
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,33 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+import { TextDecoder, TextEncoder } from "util";
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+window.seededRandom = function (seed) {
+  var m = 2 ** 35 - 31;
+  var a = 185852;
+  var s = seed % m;
+  return function () {
+    return (s = (s * a) % m) / m;
+  };
+};
+
+window.fetchAPI = function (date) {
+  let result = [];
+  let random = seededRandom(date.getDate());
+
+  for (let i = 17; i <= 23; i++) {
+    if (random() < 0.5) {
+      result.push(i + ":00");
+    }
+    if (random() < 0.5) {
+      result.push(i + ":30");
+    }
+  }
+  return result;
+};
+window.submitAPI = function (formData) {
+  return true;
+};
